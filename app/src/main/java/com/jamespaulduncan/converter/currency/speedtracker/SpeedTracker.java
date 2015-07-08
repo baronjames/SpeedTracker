a3e9fb88c6d302021ce7a8b2690207932fee65ef
James Duncan <james.paul.duncan1989@gmail.com>
2015-07-09 00:42:13 +0930

    Added androidplot.com plotting features for plotting sensor measurements

--- app/src/main/java/com/jamespaulduncan/converter/currency/speedtracker/SpeedTracker.java
+++ app/src/main/java/com/jamespaulduncan/converter/currency/speedtracker/SpeedTracker.java
@@ -2,6 +2,10 @@ package com.jamespaulduncan.converter.currency.speedtracker;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,16 +14,50 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.androidplot.util.PlotStatistics;
+import com.androidplot.xy.BarFormatter;
+import com.androidplot.xy.BarRenderer;
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
 
-public class SpeedTracker extends ActionBarActivity
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.Arrays;
+
+
+public class SpeedTracker extends ActionBarActivity implements SensorEventListener
 {
     private static final String TAG = "JD SpeedTracker";
 
     Context context;
     private TextView speedTextView;
+    private LinearLayout topLayout;
 
+    //**********Copied from http://androidplot.com/docs/dynamically-plotting-sensor-data/ ******************
+    //START GRAPHING CODE
+    private static final int HISTORY_SIZE = 30;            // number of points to plot in history
+    private SensorManager sensorMgr = null;
+    private Sensor orSensor = null;
+
+    private XYPlot aprLevelsPlot = null;
+    private XYPlot aprHistoryPlot = null;
+
+    private CheckBox hwAcceleratedCb;
+    private CheckBox showFpsCb;
+    private SimpleXYSeries aprLevelsSeries = null;
+    private SimpleXYSeries azimuthHistorySeries = null;
+    private SimpleXYSeries pitchHistorySeries = null;
+    private SimpleXYSeries rollHistorySeries = null;
+    //ENG GRAPHING CODE
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +66,7 @@ public class SpeedTracker extends ActionBarActivity
         setContentView(R.layout.activity_speed_tracker);
 
         speedTextView = (TextView) findViewById(R.id.speedTextView);
+        topLayout = (LinearLayout) findViewById(R.id.topLayout);
 
 
 
@@ -42,7 +81,7 @@ public class SpeedTracker extends ActionBarActivity
                 location.getLatitude();
                 float conversionFactor = 3.6f;
                 float kmph = location.getSpeed() * conversionFactor;
-                //Toast.makeText(context, "Current speed:" + location.getSpeed(), Toast.LENGTH_SHORT).show();
+
                 //speedTextView.setText("Speed = " + location.getSpeed() + "m/s");
                 speedTextView.setText(kmph + "k/h   " + location.getSpeed() + "m/s");
                 Log.d(TAG, "toString " + location.toString());
@@ -56,8 +95,13 @@ public class SpeedTracker extends ActionBarActivity
 
                 if (kmph > 20f)
                 {
-                    speedTextView.setBackgroundColor(Color.RED);
+                    topLayout.setBackgroundColor(Color.RED);
                 }
+                else
+                {
+                    topLayout.setBackgroundColor(Color.GREEN);
+                }
+
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras)
@@ -73,6 +117,109 @@ public class SpeedTracker extends ActionBarActivity
             }
         };
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+        //**********Copied from http://androidplot.com/docs/dynamically-plotting-sensor-data/ ******************
+        //START GRAPHING CODE
+        // setup the APR Levels plot:
+        aprLevelsPlot = (XYPlot) findViewById(R.id.aprLevelsPlot);
+
+        aprLevelsSeries = new SimpleXYSeries("APR Levels");
+        aprLevelsSeries.useImplicitXVals();
+        aprLevelsPlot.addSeries(aprLevelsSeries,
+                new BarFormatter(Color.argb(100, 0, 200, 0), Color.rgb(0, 80, 0)));
+        aprLevelsPlot.setDomainStepValue(3);
+        aprLevelsPlot.setTicksPerRangeLabel(3);
+
+        // per the android documentation, the minimum and maximum readings we can get from
+        // any of the orientation sensors is -180 and 359 respectively so we will fix our plot's
+        // boundaries to those values.  If we did not do this, the plot would auto-range which
+        // can be visually confusing in the case of dynamic plots.
+        aprLevelsPlot.setRangeBoundaries(-180, 359, BoundaryMode.FIXED);
+
+        // use our custom domain value formatter:
+        aprLevelsPlot.setDomainValueFormat(new APRIndexFormat());
+
+        // update our domain and range axis labels:
+        aprLevelsPlot.setDomainLabel("Axis");
+        aprLevelsPlot.getDomainLabelWidget().pack();
+        aprLevelsPlot.setRangeLabel("Angle (Degs)");
+        aprLevelsPlot.getRangeLabelWidget().pack();
+        aprLevelsPlot.setGridPadding(15, 0, 15, 0);
+
+        // setup the APR History plot:
+        aprHistoryPlot = (XYPlot) findViewById(R.id.aprHistoryPlot);
+
+        azimuthHistorySeries = new SimpleXYSeries("Azimuth");
+        azimuthHistorySeries.useImplicitXVals();
+        pitchHistorySeries = new SimpleXYSeries("Pitch");
+        pitchHistorySeries.useImplicitXVals();
+        rollHistorySeries = new SimpleXYSeries("Roll");
+        rollHistorySeries.useImplicitXVals();
+
+        aprHistoryPlot.setRangeBoundaries(-180, 359, BoundaryMode.FIXED);
+        aprHistoryPlot.setDomainBoundaries(0, 30, BoundaryMode.FIXED);
+        aprHistoryPlot.addSeries(azimuthHistorySeries, new LineAndPointFormatter(Color.BLUE, Color.BLACK,null, null));
+        aprHistoryPlot.addSeries(pitchHistorySeries, new LineAndPointFormatter(Color.RED, Color.BLACK,null, null));
+        aprHistoryPlot.addSeries(rollHistorySeries, new LineAndPointFormatter(Color.GREEN, Color.BLACK,null, null));
+        aprHistoryPlot.setDomainStepValue(5);
+        aprHistoryPlot.setTicksPerRangeLabel(3);
+        aprHistoryPlot.setDomainLabel("Sample Index");
+        aprHistoryPlot.getDomainLabelWidget().pack();
+        aprHistoryPlot.setRangeLabel("Angle (Degs)");
+        aprHistoryPlot.getRangeLabelWidget().pack();
+
+        // setup checkboxes:
+        hwAcceleratedCb = (CheckBox) findViewById(R.id.hwAccelerationCb);
+        final PlotStatistics levelStats = new PlotStatistics(1000, false);
+        final PlotStatistics histStats = new PlotStatistics(1000, false);
+
+        aprLevelsPlot.addListener(levelStats);
+        aprHistoryPlot.addListener(histStats);
+        hwAcceleratedCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    aprLevelsPlot.setLayerType(View.LAYER_TYPE_NONE, null);
+                    aprHistoryPlot.setLayerType(View.LAYER_TYPE_NONE, null);
+                } else {
+                    aprLevelsPlot.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                    aprHistoryPlot.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+                }
+            }
+        });
+
+        showFpsCb = (CheckBox) findViewById(R.id.showFpsCb);
+        showFpsCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                levelStats.setAnnotatePlotEnabled(b);
+                histStats.setAnnotatePlotEnabled(b);
+            }
+        });
+
+        // get a ref to the BarRenderer so we can make some changes to it:
+        BarRenderer barRenderer = (BarRenderer) aprLevelsPlot.getRenderer(BarRenderer.class);
+        if(barRenderer != null) {
+            // make our bars a little thicker than the default so they can be seen better:
+            barRenderer.setBarWidth(25);
+        }
+
+        // register for orientation sensor events:
+        sensorMgr = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        for (Sensor sensor : sensorMgr.getSensorList(Sensor.TYPE_ORIENTATION)) {
+            if (sensor.getType() == Sensor.TYPE_ORIENTATION) {
+                orSensor = sensor;
+            }
+        }
+
+        // if we can't access the orientation sensor then exit:
+        if (orSensor == null) {
+            System.out.println("Failed to attach to orSensor.");
+            cleanup();
+        }
+
+        sensorMgr.registerListener(this, orSensor, SensorManager.SENSOR_DELAY_UI);
 
     }
 
@@ -100,4 +247,76 @@ public class SpeedTracker extends ActionBarActivity
 
         return super.onOptionsItemSelected(item);
     }
+
+    //**********Copied from http://androidplot.com/docs/dynamically-plotting-sensor-data/ ******************
+    //START GRAPHING CODE
+    /**
+     * A simple formatter to convert bar indexes into sensor names.
+     */
+    private class APRIndexFormat extends Format
+    {
+        @Override
+        public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+            Number num = (Number) obj;
+
+            // using num.intValue() will floor the value, so we add 0.5 to round instead:
+            int roundNum = (int) (num.floatValue() + 0.5f);
+            switch(roundNum) {
+                case 0:
+                    toAppendTo.append("Azimuth");
+                    break;
+                case 1:
+                    toAppendTo.append("Pitch");
+                    break;
+                case 2:
+                    toAppendTo.append("Roll");
+                    break;
+                default:
+                    toAppendTo.append("Unknown");
+            }
+            return toAppendTo;
+        }
+
+        @Override
+        public Object parseObject(String source, ParsePosition pos) {
+            return null;  // We don't use this so just return null for now.
+        }
+    }
+
+    private void cleanup() {
+        // aunregister with the orientation sensor before exiting:
+        sensorMgr.unregisterListener(this);
+        finish();
+    }
+
+    // Called whenever a new orSensor reading is taken.
+    @Override
+    public synchronized void onSensorChanged(SensorEvent sensorEvent) {
+
+        // update instantaneous data:
+        Number[] series1Numbers = {sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]};
+        aprLevelsSeries.setModel(Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY);
+
+        // get rid the oldest sample in history:
+        if (rollHistorySeries.size() > HISTORY_SIZE) {
+            rollHistorySeries.removeFirst();
+            pitchHistorySeries.removeFirst();
+            azimuthHistorySeries.removeFirst();
+        }
+
+        // add the latest history sample:
+        azimuthHistorySeries.addLast(null, sensorEvent.values[0]);
+        pitchHistorySeries.addLast(null, sensorEvent.values[1]);
+        rollHistorySeries.addLast(null, sensorEvent.values[2]);
+
+        // redraw the Plots:
+        aprLevelsPlot.redraw();
+        aprHistoryPlot.redraw();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // Not interested in this event
+    }
+    //END GRAPHING CODE
 }
